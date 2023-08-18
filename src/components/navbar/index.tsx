8ade7e15c40f086d88875733efbe38eb304d5abf
RafDes21 <israeltineo84@gmail.com>
2023-08-18 20:21:58 -0300

    features in navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,46 +4,84 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineMinus } from "react-icons/ai";
 
 const NavBar = () => {
-  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const [menuOpen, setMenuOpen] = useState<boolean>(true);
 
-  const togleMenu = () => {
+  const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
   return (
-    <div className="fixed w-full z-10 text-white">
-      <nav className="bg-black">
-        <h1 className="ms-3 py-2">
+    <div className="fixed w-full z-10 h-14 flex text-white bg-black md:flex items-center md:h-20">
+      <nav className="md:flex relative px-5 max-w-screen-2xl mx-auto justify-between items-center h-auto w-full">
+        <div className="font-bold hover:text-[#fad02c] md:text-3xl">
           <Link to={"/"}>
-            El TALLER <span>CERVECERIA</span>
+            El TALLER <span className="font-light text-xs md:text-xl">CERVECERIA</span>
           </Link>
-        </h1>
-        {menuOpen ? (
-          <GiHamburgerMenu
-            onClick={togleMenu}
-            className="text-2xl cursor-pointer absolute right-2 top-2.5 "
-          />
-        ) : (
-          <AiOutlineMinus
-            onClick={togleMenu}
-            className="text-2xl  cursor-pointer absolute right-2 top-2.5 "
-          />
-        )}
-        <ul className={`${menuOpen ? "hidden":"flex flex-col justify-center items-center gap-4 h-screen"}`}>
+        </div>
+        <div className="md:hidden absolute right-5 top-1/2 -translate-y-1/2">
+          {menuOpen ? (
+            <GiHamburgerMenu
+              onClick={toggleMenu}
+              className="text-2xl cursor-pointer"
+            />
+          ) : (
+            <AiOutlineMinus
+              onClick={toggleMenu}
+              className="text-2xl cursor-pointer"
+            />
+          )}
+        </div>
+        <ul
+          className={`${
+            menuOpen
+              ? "hidden md:flex justify-center items-center gap-4"
+              : "flex flex-col md:flex-row justify-center items-center gap-4"
+          } absolute md:static bg-black bg-opacity-20 md:bg-transparent p-4 md:p-0 w-full md:w-auto mt-4 left-0 md:mt-0`}
+        >
           <li>
-            <Link to={"place"}  onClick={togleMenu}>EL LUGAR</Link>
+            <Link
+              className="hover:text-[#fad02c] font-bold text-lg"
+              to={"place"}
+              onClick={toggleMenu}
+            >
+              EL LUGAR
+            </Link>
           </li>
           <li>
-            <Link to={"menu"}  onClick={togleMenu}>EL MENÚ</Link>
+            <Link
+              className="hover:text-[#fad02c] font-bold text-lg"
+              to={"menu"}
+              onClick={toggleMenu}
+            >
+              EL MENÚ
+            </Link>
           </li>
           <li>
-            <Link to={"events"}  onClick={togleMenu}>EVENTOS</Link>
+            <Link
+              className="hover:text-[#fad02c] font-bold text-lg"
+              to={"events"}
+              onClick={toggleMenu}
+            >
+              EVENTOS
+            </Link>
           </li>
           <li>
-            <Link to={"bookings"}  onClick={togleMenu}>RESERVAS</Link>
+            <Link
+              className="hover:text-[#fad02c] font-bold text-lg"
+              to={"bookings"}
+              onClick={toggleMenu}
+            >
+              RESERVAS
+            </Link>
           </li>
           <li>
-            <Link to={"about"}  onClick={togleMenu}> NOSOTROS</Link>
+            <Link
+              className="hover:text-[#fad02c] font-bold text-lg"
+              to={"about"}
+              onClick={toggleMenu}
+            >
+              NOSOTROS
+            </Link>
           </li>
         </ul>
       </nav>
