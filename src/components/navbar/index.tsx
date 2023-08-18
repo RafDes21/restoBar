4a34e2c1cf96527a3f1eec5d68fc653c8aeb9bf3
RafDes21 <israeltineo84@gmail.com>
2023-08-18 17:53:30 -0300

    styles in navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,30 +1,49 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineMinus } from "react-icons/ai";
+
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const togleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div>
-      <nav>
-        <h1>
+    <div className="fixed w-full z-10 text-white">
+      <nav className="bg-black">
+        <h1 className="ms-3 py-2">
           <Link to={"/"}>
             El TALLER <span>CERVECERIA</span>
           </Link>
         </h1>
-        <GiHamburgerMenu />
-        <ul>
+        {menuOpen ? (
+          <GiHamburgerMenu
+            onClick={togleMenu}
+            className="text-2xl cursor-pointer absolute right-2 top-2.5 "
+          />
+        ) : (
+          <AiOutlineMinus
+            onClick={togleMenu}
+            className="text-2xl  cursor-pointer absolute right-2 top-2.5 "
+          />
+        )}
+        <ul className={`${menuOpen ? "hidden":"flex flex-col justify-center items-center gap-4 h-screen"}`}>
           <li>
-            <Link to={"place"}>EL LUGAR</Link>
+            <Link to={"place"}  onClick={togleMenu}>EL LUGAR</Link>
           </li>
           <li>
-            <Link to={"menu"}>EL MENÚ</Link>
+            <Link to={"menu"}  onClick={togleMenu}>EL MENÚ</Link>
           </li>
           <li>
-            <Link to={"events"}>EVENTOS</Link>
+            <Link to={"events"}  onClick={togleMenu}>EVENTOS</Link>
           </li>
           <li>
-            <Link to={"bookings"}>RESERVAS</Link>
+            <Link to={"bookings"}  onClick={togleMenu}>RESERVAS</Link>
           </li>
           <li>
-            <Link to={"about"}> NOSOTROS</Link>
+            <Link to={"about"}  onClick={togleMenu}> NOSOTROS</Link>
           </li>
         </ul>
       </nav>
